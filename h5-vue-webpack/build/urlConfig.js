12623ace15c5c57032a78f942a22ba02ab6915b0
张英明 <2570332082@qq.com>
2018-07-19 16:31:57 +0800

    vue+webpack demo

--- h5-vue-webpack/build/urlConfig.js
+++ h5-vue-webpack/build/urlConfig.js
@@ -1,6 +1,4 @@
 module.exports = {
-	devUrl:'http://git.nfw.com:8000/api',
-	buildUrl:'http://172.23.170.58:8090/api',
-	testUrl:'http://172.23.170.58:8090/api',
-	buildLocal:'http://172.23.170.58:8090/api'
+	devUrl:'',
+	buildUrl:''
 }
